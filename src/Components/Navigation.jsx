7eb7d25f7e94a15062f2378929f63f29080b08cc
The GreatPratyush <pratushgupta27199@gmail.com>
2026-04-18 12:31:23 +0530

    navigation bar completed

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -1,11 +1,69 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 
-const NavigationHeadings=({title,className})=>{
+const NavigationHeadings=({title,className,StateHover,dropdown})=>{
+    const [hover,setHover]=useState(false)
     return (
-        <div className={className}>{title}</div>
+        <div className={className} onMouseEnter={()=>{setHover(true)}} onMouseLeave={()=>{setHover(false)}}>
+            
+            {title}{dropdown&&"⌄"}
+            {hover&&StateHover&&<StateHover/>}
+       
+        </div>
     )
 }
+
+
+
+
+const OnHoverNavigationHeading=({arr})=>{
+
+    return(
+        <div className='questHover'>
+            <ul>
+                {arr.map((e)=>{
+                    return <li key={e}>{e}</li>
+                })}
+            </ul>
+        </div>
+    )
+}
+
+const onHoverSafety=()=>{
+    return(
+        <div  className='questHover'>
+            <table>
+                <thead>
+                <tr>
+                    <th>Resources</th>
+                    <th>Hubs</th>
+                </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Family Center</td>
+                        <td>Parent Hub</td>
+                    </tr>
+                    <tr>
+                        <td>Safety Library</td>
+                        <td>Policy Hub</td>
+                    </tr>
+                    <tr>
+                        <td>Safety News</td>
+                        <td>Privacy Hub</td>
+                    </tr>
+                    <tr>
+                        <td>Teen Charter</td>
+                        <td>Transparency Hub</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+
+
 const Navigation = () => {
   return ( 
     <div className='navigationBar'>
@@ -15,13 +73,67 @@ const Navigation = () => {
         <div className='navItem'>
 
             <NavigationHeadings title="Downloads" className="navigationHeadings" />
+
+
+
+
             <NavigationHeadings title="Nitro" className="navigationHeadings"/>
+
+
+
+
+
             <NavigationHeadings title="Discover" className="navigationHeadings"/>
-            <NavigationHeadings title="Safety" className="navigationHeadings"/>
-            <NavigationHeadings title="Quests" className="navigationHeadings"/>
-            <NavigationHeadings title="Support" className="navigationHeadings"/>
-            <NavigationHeadings title="Blog" className="navigationHeadings"/>
-            <NavigationHeadings title="Developer" className="navigationHeadings" />
+
+
+
+
+            <NavigationHeadings title="Safety" className="navigationHeadings" StateHover={onHoverSafety} dropdown={true}/>
+
+
+
+            <NavigationHeadings title="Quests" className="navigationHeadings" 
+            
+                StateHover={()=>{
+                    return(
+                        <OnHoverNavigationHeading arr={["Resources","Advertising","Success Stories","Quests FAQ"]}/>
+                    )
+                }} dropdown={true}/>
+
+
+
+            <NavigationHeadings title="Support" className="navigationHeadings"
+
+                StateHover={()=>{
+                    return(
+                        <OnHoverNavigationHeading arr={["Resources","Help Centre","Feedback","Submit a Response"]}/>
+                    )
+                }} dropdown={true}
+            />
+
+
+
+            <NavigationHeadings title="Blog" className="navigationHeadings"
+                StateHover={()=>{return(
+                    <OnHoverNavigationHeading arr={["Collections","Featured","Community","Discord HQ","Engineering & Developers","How to Discord","Policy & Safety","Product & Features"]}/>
+                )
+                }} dropdown={true}
+            />
+
+
+
+            <NavigationHeadings title="Developer" className="navigationHeadings" 
+                StateHover={()=>{
+                    return(
+                        <OnHoverNavigationHeading arr={["Build","Official Game Communites","Developer Portal","Documentation","Developer Help Center"]}/>
+                    )
+                }}
+                dropdown={true}
+            />
+
+
+
+
             <NavigationHeadings title="Careers" className="navigationHeadings"/>
 
         </div>
